585b158480143c6e3c9b3319edf1065fe8f73d55
GabrielPedraga <107312107+GabrielPedraga@users.noreply.github.com>
2022-12-16 07:01:19 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import './logo1.jpg'
+
+function Blog(){
+  return(
+    <div className='main'> 
+      <h2>Notícia</h2>
+      <h1>Subtitulo</h1>
+      <p>Sobre a notícia</p>
+      <h4>Tempo</h4>
+      <img src="./logo1.jpg"></img>
+
+    </div>
+    
+  );
+}
+
+export default Blog;
